050914a8c64ea0a03257b9dd74710470ad785d44
easy4me <j.diazlop90@gmail.com>
2020-11-15 17:58:53 +0100

    Añadir test unitarios para las vistas del juego de las damas con Mockito

--- solution.Java/src/test/java/usantatecla/draughts/AllTests.java
+++ solution.Java/src/test/java/usantatecla/draughts/AllTests.java
@@ -5,11 +5,13 @@ import org.junit.runners.Suite;
 import org.junit.runners.Suite.SuiteClasses;
 import usantatecla.draughts.controllers.AllControllerTest;
 import usantatecla.draughts.models.AllModelTests;
+import usantatecla.draughts.views.AllViewTests;
 
 @RunWith(Suite.class)
 @SuiteClasses({
         AllModelTests.class,
-        AllControllerTest.class
+        AllControllerTest.class,
+        AllViewTests.class
 })
 public class AllTests {
 
